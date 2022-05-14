718ce7b33f317181158b614cd71c32b7115ec6c8
dima forostenko <forostenkochampion@gmail.com>
2022-05-14 22:42:54 +0300

    'add commit 6 task'

---
+++ index.js
@@ -0,0 +1,41 @@
+
+//встроенный обьект contact//объединил два массива//
+
+const array1=[1,2,3];
+const array2=[4,5,6];
+
+const array3=array1.concat(array2);
+console.log(array3);
+
+//встроенный обьект reverse//изменил порядок массива/
+
+const number = [1,2,3];
+number.reverse();
+
+console.log(number);
+
+//встроенный обьект push//добавил елменты в налало массива
+
+const and =[1,2,3];
+const next = and.push[4,5,6];
+
+console.log(and);
+
+//встроенный обьект unshift//добвавил элементы в нонец массива
+
+const num=[1,2,3];
+num.unshift(4,5,6);
+
+console.log(num);
+
+//встроенный элемент shift//встроенный элемент pop//
+//удалил первый элемент и вывел его// удалил последний елемент и вывел его//
+const ele = ['js','css','html'];
+console.log(ele);
+const shifted = ele.shift();
+const poped = ele.pop();
+console.log(shifted);
+console.log(poped);
+
+
+
