5f12e2f4cc3a82201c8296a3bd3caf592e224201
dima forostenko <forostenkochampion@gmail.com>
2022-05-26 00:07:48 +0300

    'add randomNumb'

--- index.js
+++ index.js
@@ -1,88 +1,96 @@
-
 //встроенный обьект contact//объединил два массива//
 
-const array1=[1,2,3];
-const array2=[4,5,6];
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
 
-const array3=array1.concat(array2);
+const array3 = array1.concat(array2);
 console.log(array3);
 
 //встроенный обьект reverse//изменил порядок массива/
 
-const number = [1,2,3];
+const number = [1, 2, 3];
 number.reverse();
 
 console.log(number);
 
 //встроенный обьект push//добавил елменты в налало массива
 
-const and =[1,2,3];
-const next = and.push[4,5,6];
+const and = [1, 2, 3];
+const next = and.push[(4, 5, 6)];
 
 console.log(and);
 
 //встроенный обьект unshift//добвавил элементы в нонец массива
 
-const num=[1,2,3];
-num.unshift(4,5,6);
+const num = [1, 2, 3];
+num.unshift(4, 5, 6);
 
 console.log(num);
 
 //встроенный элемент shift//встроенный элемент pop//
 //удалил первый элемент и вывел его// удалил последний елемент и вывел его//
-const ele = ['js','css','html'];
+const ele = ["js", "css", "html"];
 console.log(ele);
 const shifted = ele.shift();
 const poped = ele.pop();
 console.log(shifted);
 console.log(poped);
 
-
 // метод slice возвращает новый массив с часть исходного//
-const NumArray = [1,2,3,4,5];
-const newNumArray = NumArray.slice(0,-2);
-console.log (newNumArray);
+const NumArray = [1, 2, 3, 4, 5];
+const newNumArray = NumArray.slice(0, -2);
+console.log(newNumArray);
 const newNumArray2 = NumArray.slice(3);
-console.log (newNumArray2) ;
+console.log(newNumArray2);
 
 //метод splice ///
-const removed = NumArray.splice(1,2);
-console.log (NumArray);
-const NumArray2 = [1,2,3,4,5];
-const rem = NumArray2.splice(1,3);
+const removed = NumArray.splice(1, 2);
+console.log(NumArray);
+const NumArray2 = [1, 2, 3, 4, 5];
+const rem = NumArray2.splice(1, 3);
 console.log(rem);
-const NumArray3 = [1,2,3,4,5];
-let removed2=NumArray3.splice(3,0,'a','b','c');
+const NumArray3 = [1, 2, 3, 4, 5];
+let removed2 = NumArray3.splice(3, 0, "a", "b", "c");
 console.log(NumArray3);
-const NumArray4 = [1,2,3,4,5];
-let removed3=NumArray4.splice(1,0,'a','b');
+const NumArray4 = [1, 2, 3, 4, 5];
+let removed3 = NumArray4.splice(1, 0, "a", "b");
 console.log(NumArray4);
 
 ////  метод sort ///
 
-const ArrayNumb=[3,4,1,2,7];
-function compareNumbers(a,b){
-  return a-b;
-};
+const ArrayNumb = [3, 4, 1, 2, 7];
+function compareNumbers(a, b) {
+  return a - b;
+}
 console.log(ArrayNumb.sort(compareNumbers));
 //////////////////////////1
-"use strict";
-function hasElem(value){
-  return typeof value ==='';
+("use strict");
+function hasElem(value) {
+  return typeof value === "";
 }
-const StArr1 = [1,2,3];
+const StArr1 = [1, 2, 3];
 console.log(StArr1.every(hasElem));
-  // const HomWork = function hasElem(string,array){
-  // if('' === array){
-  //     return true;
-  // }
-  // if('' !== array ){
-  //     return false
-  // };
-  // }
-// 
+
 ///////////////////////////////////2
-const homWork2=[1,5,9,8,4,6,1,2];
-const result2=homWork2.includes(2);
+const homWork2 = [1, 5, 9, 8, 4, 6, 1, 2];
+const result2 = homWork2.includes(2);
 console.log(result2);
 ////////////////////////////////3
+// искать;
+////////////////////////////////4
+const random_arr=function() {
+  let lenght=10;
+  const min = 5;
+  const max = 33;
+  const resultRan = [];
+  for (let i = 0; i < 1; i++) {
+    resultRan.push(RandomNumb(min, max));
+  }
+  console.log(resultRan);
+  return resultRan;
+};
+function RandomNumb(min, max) {
+  let rand = Math.random() * (max - min )+ min;
+  rand = Math.random(rand);
+  return rand;
+}
